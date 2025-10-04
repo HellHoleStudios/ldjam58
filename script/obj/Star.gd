f5e4f7d148ab44d9b6fa690c1ca549f71ad24b89
melon6826 <2795178962@qq.com>
2025-10-04 19:03:23 +0800

    优化星体碰撞逻辑，使天体之间可以吞食，添加对玩家星体的特殊处理

--- script/obj/Star.gd
+++ script/obj/Star.gd
@@ -9,15 +9,18 @@ extends RigidBody2D
 	#update_visual()
 
 func _ready() -> void:
-	$Collision.shape=CircleShape2D.new()
+	$Collision.shape = CircleShape2D.new()
 	update_visual()
+
+	contact_monitor = true
+	max_contacts_reported = 4
 
 func update_mass(_mass: float):
 	mass = _mass
 	update_visual()
 
 func get_radius() -> float:
-	return sqrt(mass)*3
+	return sqrt(mass) * 3
 
 func get_sprite() -> Sprite2D:
 	return $Sprite
@@ -27,4 +30,20 @@ func update_visual():
 	$Sprite.scale = Vector2(1, 1) * radius / 32
 	$Collision.shape.radius = radius
 	
-	
+
+func _on_body_entered(body: Node) -> void:
+	if body is Star:
+		var star: Star = body
+		if star.get_mass() < mass:
+			if star is not PlayerStar:
+				self.update_mass(star.mass + mass)
+				star.queue_free()
+				print("Sucked!!! New mass:", mass)
+			else:
+				var BOUNCE_FACTOR = 4
+				var dir = (star.position - position).normalized()
+				# 连线方向上的分速度设为0
+				var rel_vel = star.linear_velocity - linear_velocity
+				var proj = rel_vel.dot(dir)
+				star.linear_velocity -= dir * proj * BOUNCE_FACTOR
+				linear_velocity += dir * proj * BOUNCE_FACTOR
