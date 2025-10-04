ae89e97c934ceb7d78965e57bd262d0e240bd040
Zzzyt <Zzzyt205@gmail.com>
2025-10-04 16:57:20 +0800

    better gravity

--- script/obj/BaseStarData.gd
+++ script/obj/BaseStarData.gd
@@ -7,50 +7,52 @@ var collision_shape: CollisionShape2D
 var rigidbody: RigidBody2D
 
 func _init(_mass: float = 1.0, _sprite: Sprite2D = null, _collision_shape: CollisionShape2D = null, _rigidbody: RigidBody2D = null):
-    mass = _mass
-    sprite = _sprite
-    collision_shape = _collision_shape
-    rigidbody = _rigidbody
-    if sprite and collision_shape:
-        update_visual()
+	mass = _mass
+	sprite = _sprite
+	collision_shape = _collision_shape
+	rigidbody = _rigidbody
+	if sprite and collision_shape:
+		update_visual()
 
 func get_mass() -> float:
-    return mass
+	return mass
 
 func set_mass(_mass: float):
-    mass = _mass
-    update_visual()
+	mass = _mass
+	update_visual()
 
 func get_radius() -> float:
-    return mass
+	return sqrt(mass)*3
 
 func update_visual():
-    var radius = get_radius()
-    if sprite:
-        sprite.scale = Vector2(1, 1) * radius / 32
-    if collision_shape and collision_shape.shape is CircleShape2D:
-        collision_shape.shape.radius = radius
+	var radius = get_radius()
+	if sprite:
+		sprite.scale = Vector2(1, 1) * radius / 32
+	if collision_shape and collision_shape.shape is CircleShape2D:
+		collision_shape.shape.radius = radius
+	if rigidbody:
+		rigidbody.mass = mass
 
 func set_sprite(_sprite: Sprite2D):
-    sprite = _sprite
-    update_visual()
+	sprite = _sprite
+	update_visual()
 
 func set_collision_shape(_collision_shape: CollisionShape2D):
-    collision_shape = _collision_shape
-    update_visual()
+	collision_shape = _collision_shape
+	update_visual()
 
 func set_rigidbody(_rigidbody: RigidBody2D):
-    rigidbody = _rigidbody
+	rigidbody = _rigidbody
 
 func get_sprite() -> Sprite2D:
-    return sprite
+	return sprite
 
 func get_collision_shape() -> CollisionShape2D:
-    return collision_shape
+	return collision_shape
 
 func get_rigidbody() -> RigidBody2D:
-    return rigidbody
+	return rigidbody
 
 func apply_force(force: Vector2):
-    if rigidbody:
-        rigidbody.apply_central_impulse(force)
+	if rigidbody:
+		rigidbody.apply_central_impulse(force)
