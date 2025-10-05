1444a8e53e0a430e90aaa2716f692e6a85327fb1
Zzzyt <Zzzyt205@gmail.com>
2025-10-05 21:03:20 +0800

    byebye elements

--- script/ui/data_container.gd
+++ script/ui/data_container.gd
@@ -5,18 +5,4 @@ func _process(delta: float) -> void:
 	$Pos.text="(%.1f,%.1f) Layer %d"%[%player.position.x, %player.position.y, Game.get_layer(%player.position)]
 	$Mass.text="%.3f M⨀"%(%player.mass)
 	$Merge.text="%d times"%(%player.merge_count)
-	for i in %player.elements:
-		if i not in elements:
-			elements.append(i)
-			
-			var lbl=Label.new()
-			lbl.text=i
-			add_child(lbl)
-			
-			var lbl2=Label.new()
-			lbl2.name=i
-			lbl2.text="UWU"
-			add_child(lbl2)
-	
-	for i in elements:
-		get_node(i).text="%.3f M⨀"%[%player.elements[i]]
+	$Age.text="%.0f%%"%(%player.age*100)
