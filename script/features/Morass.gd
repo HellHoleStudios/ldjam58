fb17f1dfce0470955675b4191df90898de3d79dd
melon6826 <2795178962@qq.com>
2025-10-06 10:55:47 +0800

    使feature都不会影响到自己的子弹

--- script/features/Morass.gd
+++ script/features/Morass.gd
@@ -18,7 +18,7 @@ func process(delta: float) -> void:
         sprite.z_index = -1
 
     # 半透明，随时间波动
-    sprite.modulate.a = 0.3 + 0.1 * sin(timer * 5)
+    sprite.modulate.a = 0.3 + 0.04 * sin(timer * 5)
 
     var affect_range: float = star.get_radius() * 5
     sprite.scale = Vector2.ONE * affect_range / (sprite_texture.get_height() / 2)
@@ -26,8 +26,16 @@ func process(delta: float) -> void:
     # 对范围内所有星体自己加一层阻尼
     for other_star in Game.get_all_stars():
         var other: Star = other_star
+        # 遍历features，检查是否有Bullet且father是自己，不会影响到自己发射的子弹
+        var has_bullet = false
+        for f in other.features:
+            if f is Bullet and f.father == star:
+                has_bullet = true
+                break
+        if has_bullet:
+            continue
         if other != star and other.get_position().distance_to(star.get_position()) < affect_range:
-            other.linear_velocity *= 0.95
+            other.linear_velocity *= 0.97
 
 static func generate_weight(stars: Array[Node], player: PlayerStar, star: Star) -> float:
     return 0.02
